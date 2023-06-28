ed909eb62c13d9693e5a59562c012ca407bc946c
Isabelle <loariyafiaba@gmail.com>
2023-06-28 10:38:56 +0100

    Started the magic ball 8 project

---
+++ magic-ball-8/main.py
@@ -0,0 +1,10 @@
+import random
+
+name = "Tracey"
+question = "Would you like to break up with your fiance? "
+answer = ""
+
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+  print("Yes - definitely")
