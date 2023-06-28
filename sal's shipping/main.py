08fad385e2818a8e8ccdbdfc01c3cce3eed1723d
Isabelle <loariyafiaba@gmail.com>
2023-06-28 13:28:21 +0100

    Added the drone shipping control flow

--- sal's shipping/main.py
+++ sal's shipping/main.py
@@ -22,4 +22,22 @@ else:
   cost = (weight * price_per_pound) + ground_shipping_flat_charge
   print("$" + str(cost))
 
+# Drone Shipping
+drone_shipping_flat_charge = 0.00
 
+if weight <= 2:
+  price_per_pound = 4.50
+  cost = (weight * price_per_pound) + drone_shipping_flat_charge
+  print("$" + str(cost))
+elif weight > 2 and weight <= 6:
+  price_per_pound = 9.00
+  cost = (weight * price_per_pound) + drone_shipping_flat_charge
+  print("$" + str(cost))
+elif weight > 6 and weight <= 10:
+  price_per_pound = 12.00
+  cost = (weight * price_per_pound) + drone_shipping_flat_charge
+  print("$" + str(cost))
+else:
+  price_per_pound = 14.25
+  cost = (weight * price_per_pound) + drone_shipping_flat_charge
+  print("$" + str(cost))
