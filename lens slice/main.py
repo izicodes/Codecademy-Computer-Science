01d51e7296e092598905b99ad922e6aa0acd9dfa
Isabelle <loariyafiaba@gmail.com>
2023-06-29 12:32:16 +0100

    Used .count() and len() on the lists

--- lens slice/main.py
+++ lens slice/main.py
@@ -1,3 +1,13 @@
 toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
 
 prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+
+num_pizzas = len(toppings)
+
+print("We sell", num_pizzas, "different kinds of pizza!")
+
+pizza_and_prices = [["pepperoni", 2], ["pineapple", 6], ["cheese", 1], ["sausage", 3], ["olives", 2], ["olives", 7], ["mushrooms", 2]]
+
+print(num_pizzas)
