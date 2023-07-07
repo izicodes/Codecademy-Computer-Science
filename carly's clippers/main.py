8ce4d5261ecd513326c1d1dec1b868aa3e8bacf1
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-07-07 10:58:03 +0100

    Made a loop through prices and getting an average

--- carly's clippers/main.py
+++ carly's clippers/main.py
@@ -3,3 +3,12 @@ hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk"
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# ---------------  MY CODE   ---------------
+
+total_price = 0
+
+for price in prices:
+  total_price += price
+
+average_price = total_price / len(prices)
