c4e40d7b5aaebe2f939d670e132ee9aba07f2e1e
Isabelle <loariyafiaba@gmail.com>
2023-07-08 22:34:28 +0100

    Added the initial code

---
+++ physics class/main.py
@@ -0,0 +1,9 @@
+# Uncomment this when you reach the "Use the Force" section
+# train_mass = 22680
+# train_acceleration = 10
+# train_distance = 100
+# bomb_mass = 1
+
+
+# Write your code below: 
+
