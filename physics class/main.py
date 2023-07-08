fee341d4719b245c891f4ac616dee336bf259bbb
Isabelle <loariyafiaba@gmail.com>
2023-07-08 23:09:23 +0100

    Made force and energy functions

--- physics class/main.py
+++ physics class/main.py
@@ -1,8 +1,8 @@
 # Uncomment this when you reach the "Use the Force" section
-# train_mass = 22680
-# train_acceleration = 10
-# train_distance = 100
-# bomb_mass = 1
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
 
 
 # Write your code below: 
@@ -15,6 +15,16 @@ def c_to_f(c_temp):
   f_temp = c_temp * (9/5) + 32
   return f_temp
 
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+def get_energy(mass, c=3*10**8):
+  return mass * (c*c)
+
 f100_in_celsius = f_to_c(100)
 
 c0_in_fahrenheit = c_to_f(0)
+
+train_force = get_force(train_mass, train_acceleration)
+
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
