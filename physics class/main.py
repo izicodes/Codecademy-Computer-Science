0a0919a201abf2f5e1cbadf60ab6d45586e68351
Isabelle <loariyafiaba@gmail.com>
2023-07-08 22:40:09 +0100

    Created functions for the two temp. units

--- physics class/main.py
+++ physics class/main.py
@@ -7,3 +7,14 @@
 
 # Write your code below: 
 
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * (5/9)
+  return c_temp
+
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+
+f100_in_celsius = f_to_c(100)
+
+c0_in_fahrenheit = c_to_f(0)
